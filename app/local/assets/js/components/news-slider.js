3ca6d56feb2e3ae2b287678d81ec43b0b707d137
Nail <nailgalimov8@gmail.com>
2022-02-05 14:26:21 +0300

    upd

---
+++ app/local/assets/js/components/news-slider.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",(function(e){new Swiper(".js-news-slider",{slidesPerView:3,spaceBetween:32,breakpoints:{310:{slidesPerView:1.3,spaceBetween:16},767:{slidesPerView:2,spaceBetween:16},991:{slidesPerView:3,spaceBetween:32}}})}));
--- src/js/components/intro-block-slider.js
+++ src/js/components/intro-block-slider.js
@@ -1,1 +1,58 @@
-console.log("123");
+document.addEventListener("DOMContentLoaded", function (event) {
+	const introSlider = new Swiper('.js-intro-slider', {
+		slidesPerView: 1,
+        spaceBetween: 10,
+		navigation: {
+			nextEl: '.js-intro-slider-next',
+			prevEl: '.js-intro-slider-prev',
+		},
+		autoplay: {
+			delay: 9000,
+			disableOnInteraction: false,
+		},
+		on: {
+			afterInit: function(swiper) {
+				let btn = document.querySelector(".intro-slider__nav");
+
+				btn.classList.add("anim-start");
+			},
+			slideChange: function (swiper) {
+				let btn = document.querySelector(".intro-slider__nav");
+
+				btn.classList.remove("anim-start");
+				setTimeout(() => {
+					btn.classList.add("anim-start");
+				});
+
+				if (swiper.realIndex == 0) {
+					let btn = document.querySelector(".intro-slider__nav");
+
+					btn.classList.remove("anim-start");
+					setTimeout(() => {
+						btn.classList.add("anim-start");
+					});
+				}
+			},
+			slideNextTransitionStart: function (swiper) {
+				if (swiper.realIndex == 0) {
+					document.querySelectorAll(".slider-nav__btn").forEach((btn)=>{btn.classList.remove("opacity");btn.classList.remove("non-opacity");});
+				} else {
+					document.querySelectorAll(".slider-nav__btn").forEach((btn)=>{btn.classList.remove("opacity");});
+					swiper.navigation.prevEl.classList.add("opacity");
+				}
+			},
+			slidePrevTransitionStart: function (swiper) {
+				if (swiper.realIndex == 0) {
+					document.querySelectorAll(".slider-nav__btn").forEach((btn)=>{btn.classList.remove("opacity");btn.classList.remove("non-opacity");});
+				} else {
+
+					document.querySelectorAll(".slider-nav__btn").forEach((btn)=>{btn.classList.remove("opacity");});
+					swiper.navigation.nextEl.classList.add("opacity");
+				}
+			},
+			reachEnd: function (swiper) {
+				swiper.navigation.prevEl.classList.add("non-opacity");
+			}
+		}
+	});
+});
