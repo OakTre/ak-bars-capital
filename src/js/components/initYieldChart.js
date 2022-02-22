8ba1048c1291b7b8d5c14b658d488b08a64506af
Nail <nailgalimov8@gmail.com>
2022-02-22 12:25:48 +0300

    upd

--- src/js/components/initYieldChart.js
+++ src/js/components/initYieldChart.js
@@ -1,70 +1,108 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 	const btnsInit = document.querySelectorAll(".js-init-chart");
-	let myChart;
+	const charts = document.querySelectorAll(".js-yield-info-chart");
 
-	const labels = [
-		'янв `21',
-		'фев `21',
-		'мар `21',
-		'апр `21',
-		'май `21',
-		'июн `21',
-		'июл `21',
-		'авг `21',
-		'сен `21',
-		'окт `21',
-		'ноя `21',
-		'дек `21',
-	];
+	let myCharts = [];
 
-	const data = {
-		labels: labels,
-		datasets: [{
-			label: false,
-			borderColor: 'rgba(0, 178, 169, 1)',
-			fill: true,
-			backgroundColor: gradient,
-			data: [40, 10, 5, 2, 20, 30, 30, 15, 21, 60, 42, 12],
-		}]
-	};
+	let dateText = document.querySelector(".js-date-text");
+	const chartDates = document.querySelectorAll(".js-yield-info-dates");
 
-	const config = {
-		type: 'line',
-		data: data,
-		options: {
-			responsive: true,
-			maintainAspectRatio: false,
-			plugins: {
-				legend: {
-					display: false
-				}
-			},
-			elements: { point: { radius: 0 } }
+	// локализация для дат
+	dayjs.locale('ru');
+	// подставляем текущую дату
+	dateText.innerHTML = dayjs(new Date()).format('DD MMMM YYYY');
+
+	// инициализируем дайтпикер
+	new AirDatepicker('.js-dates', {
+		maxDate: new Date(),
+		onSelect({date, formattedDate, datepicker}) {
+			dateText.innerHTML = dayjs(date).format('DD MMMM YYYY');
 		}
-	};
+	})
+
+	// проходимся по всем датам в фондах и инициализируем дейтпикер
+	chartDates.forEach((date)=>{
+		const parent = date.closest(".js-dates-parent");
+		const startDate = parent.querySelector(".js-start-date");
+		const endtDate = parent.querySelector(".js-end-date");
+
+		new AirDatepicker(date, {
+			range: true,
+			multipleDatesSeparator: ' - ',
+			maxDate: new Date(),
+			onSelect({date, formattedDate, datepicker}) {
+				for (let i =0; i<date.length; i++) {
+					startDate.innerHTML = dayjs(date[0]).format('DD MMMM YYYY');
+					endtDate.innerHTML = dayjs(date[1]).format('DD MMMM YYYY');
+				}
+			}
+		})
+	});
+
+	// проходимся по всем фондам и инициализируем графики
+	charts.forEach((chart) => {
+		const ctx = chart.getContext("2d");
+		const gradient = ctx.createLinearGradient(0, 0, 0, 380);
+		gradient.addColorStop(0, 'rgba(0, 178, 169, 0.2)');
+		gradient.addColorStop(1, 'rgba(0, 178, 169, 0)');
+
+		const labels = [
+			dayjs(new Date()).format(' MMM YY'),
+			'фев `21',
+			'мар `21',
+			'апр `21',
+			'май `21',
+			'июн `21',
+			'июл `21',
+			'авг `21',
+			'сен `21',
+			'окт `21',
+			'ноя `21',
+			'дек `21',
+		];
+
+		const data = {
+			labels: labels,
+			datasets: [{
+				label: false,
+				borderColor: 'rgba(0, 178, 169, 1)',
+				fill: true,
+				backgroundColor: gradient,
+				data: [40, 10, 5, 2, 20, 30, 30, 15, 21, 60, 42, 12],
+			}]
+		};
+
+		const config = {
+			type: 'line',
+			data: data,
+			options: {
+				responsive: true,
+				maintainAspectRatio: false,
+				plugins: {
+					legend: {
+						display: false
+					}
+				},
+				elements: { point: { radius: 0 } }
+			}
+		};
+
+		let chartNew = new Chart(
+			ctx,
+			config
+		);
+
+		myCharts.push(chartNew);
+	});
 
 
 	btnsInit.forEach((btn) => {
 		btn.addEventListener("click", () => {
-			const ctx = btn.closest(".js-chart-parent").querySelector(".js-yield-info-chart").getContext("2d");
-			const gradient = ctx.createLinearGradient(0, 0, 0, 380);
-
-			gradient.addColorStop(0, 'rgba(0, 178, 169, 0.2)');
-			gradient.addColorStop(1, 'rgba(0, 178, 169, 0)');
+			// какие-то действия для обновления графиков, хз может запрос какой-то
 
 
-			if (btn.className.includes('js-chart-is-active')) {
-				btn.classList.remove("js-chart-is-active");
-				myChart.destroy();
-			} else {
-				btnsInit.forEach(el=>{el.classList.remove("js-chart-is-active")});
-				btn.classList.add("js-chart-is-active");
 
-				myChart = new Chart(
-					ctx,
-					config
-				);
-			}
+			// myChart.update();
 		});
 	});
 });
