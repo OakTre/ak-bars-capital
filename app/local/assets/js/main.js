a7576753b460264c82efa2bf4a002e99cdfd9a74
Nail <nailgalimov8@gmail.com>
2022-02-25 18:22:33 +0300

    upd

--- app/local/assets/js/main.js
+++ app/local/assets/js/main.js
@@ -17,20 +17,21 @@ window.onload = () => {
 function toTab() {
 	const tab = document.querySelector("#tab");
 	const jsPeymtnTabs = document.querySelectorAll(".js-payment-tab");
+	const tabParam = getParams().tabs;
 
 	if (tab) {
-		if (getParams().tabs.length) {
+		if (tabParam) {
 			$('html, body').animate({
 				scrollTop: $("#tab").offset().top
 			}, 900);
 
 			let tabId = getParams().tabs;
 
-			jsPeymtnTabs.forEach((tab)=>{
+			jsPeymtnTabs.forEach((tab) => {
 				let tabAttr = tab.dataset.paymentTab;
 
 				if (tabAttr == tabId) {
-					jsPeymtnTabs.forEach((el)=>{el.querySelector(".js-tab").checked = false});
+					jsPeymtnTabs.forEach((el) => { el.querySelector(".js-tab").checked = false });
 
 					tab.querySelector(".js-tab").checked = true;
 				}
@@ -82,18 +83,43 @@ function getParams() {
 // якорный скролл
 function scrollByAnchor() {
 	let headerHeight = getComputedStyle(document.documentElement).getPropertyValue('--header-height');
-	headerHeight = headerHeight.replace(/[^0-9]/g,"");
-	headerHeight+="0"
+	headerHeight = headerHeight.replace(/[^0-9]/g, "");
+	headerHeight += "0"
 
-    $("a[href^='#']").click(function() {
-        const _href = $(this).attr('href');
-        $('html, body').animate({ scrollTop: $(_href).offset().top - Number(headerHeight) + 'px' }, 800);
-        return false;
-    });
+	$("a[href^='#']").click(function () {
+		const _href = $(this).attr('href');
+		$('html, body').animate({ scrollTop: $(_href).offset().top - Number(headerHeight) + 'px' }, 800);
+		return false;
+	});
 }
 
+// обработчик запроса
+function sendRequest(method, url) {
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
 
-document.addEventListener("DOMContentLoaded", function(event) {
+		xhr.open(method, url);
+
+		xhr.responseType = "json";
+
+		xhr.onload = () => {
+			if (xhr.status >= 400) {
+				reject(xhr.response);
+			} else {
+				resolve(xhr.response);
+			}
+		};
+
+		xhr.onerror = () => {
+			reject(xhr.response);
+		};
+
+		xhr.send();
+	});
+};
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
 	const options = BodyScrollOptions = {
 		reserveScrollBarGap: true,
 	};
@@ -129,7 +155,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				y: y,
 				autoAlpha: 0
 			}, {
-				delay: 0.4,
+				scrollTrigger:{
+					trigger: elem
+				},
+				delay: 0.3,
 				duration: 1.25,
 				y: 0,
 				autoAlpha: 1,
@@ -144,10 +173,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				autoAlpha: 0
 			});
 
+			animFrom(elem);
+
 			ScrollTrigger.create({
 				trigger: elem,
 				onEnter: function () {
-					animFrom(elem);
+					// animFrom(elem);
 				},
 				onEnterBack: function () {
 					// animFrom(elem, -1);
@@ -165,17 +196,94 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	const mobileMenuBtn = document.querySelector(".header__mobile-btn");
 	const mobileMenuCloseBtn = document.querySelector(".mobile-menu__close-btn");
 
-	mobileMenuBtn.addEventListener("click", ()=>{
+	mobileMenuBtn.addEventListener("click", () => {
 		mobileMenu.classList.add("is-open");
 		disableScroll();
 
 		// bodyScrollLock.disableBodyScroll(mobileMenu, options);
 	});
 
-	mobileMenuCloseBtn.addEventListener("click", ()=>{
+	mobileMenuCloseBtn.addEventListener("click", () => {
 		mobileMenu.classList.remove("is-open");
 		enableScroll();
 
 		// bodyScrollLock.enableBodyScroll(mobileMenu, options);
+	});
+
+	const modal = new Modal({
+		isOpen: (modal) => {
+			let btn = modal.previousActiveElement;
+			let attr = btn.dataset.modalHeading;
+			let modalHeadinng = modal.modalContainer.querySelector(".modal__heading");
+
+			if (attr) {
+				modalHeadinng.innerHTML = attr;
+			} else {
+				modalHeadinng.innerHTML = "Обращение в компанию";
+			}
+		},
+		isClose: (modal) => {
+			let form = modal.modalContainer.querySelector("form");
+
+			if (form) {
+				let inpts = form.querySelectorAll("input");
+
+				inpts.forEach((inpt)=>{
+					inpt.value = "";
+
+					if (inpt.closest("label").querySelector("ul.parsley-errors-list")) {
+						inpt.closest("label").querySelector("ul.parsley-errors-list").remove();
+					}
+
+					inpt.classList.remove("parsley-error");
+				});
+
+				form.querySelector(".form__agreement").classList.remove("parsley-error");
+			}
+		},
+	});
+
+
+
+	// отпарвка форм
+	const forms = document.querySelectorAll("form:not([data-custom-request])");
+
+	forms.forEach((form)=>{
+		form.addEventListener("submit", (e)=>{
+			e.preventDefault();
+			let url = form.getAttribute("action");
+
+			sendRequest("GET", url)
+				.then((data)=>{
+					if (modal.isOpen === true) {
+						modal.onClose("callback");
+						modal.onOpen("succes");
+
+						let inpts = form.querySelectorAll("input");
+
+						inpts.forEach((inpt)=>{
+							inpt.value = "";
+							inpt.classList.remove("parsley-error");
+						});
+					} else {
+						modal.onOpen("succes");
+
+						let inpts = form.querySelectorAll("input");
+
+						inpts.forEach((inpt)=>{
+							inpt.value = "";
+							inpt.classList.remove("parsley-error");
+						});
+					}
+				})
+				.catch(() => {
+					if (modal.isOpen === true) {
+						modal.onClose("callback");
+						modal.onOpen("error");
+					} else {
+						modal.onOpen("error");
+					}
+				})
+		});
 	});
 });
